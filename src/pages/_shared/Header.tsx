90a745f750ec5bf0e5920b06adba6fbc82fd7852
bugeon <bearbu@naver.com>
2022-10-22 15:33:50 +0900

    design: set Header scss

--- src/pages/_shared/Header.tsx
+++ src/pages/_shared/Header.tsx
@@ -1,29 +1,29 @@
+import { Link, NavLink } from 'react-router-dom'
 import classNames from 'classnames/bind'
 
-import styles from './Header.module.scss'
-
-import { NavLink } from 'react-router-dom'
+import styles from './header.module.scss'
 
 const cx = classNames.bind(styles)
 
-const MENUS = [
-  { title: '회사소개', to: 'introduction' },
-  { title: '봉안(납골)당', to: '' },
-  { title: '수목장(자연 및 평장)', to: '' },
-  { title: '봉안(납골)묘', to: '' },
-  { title: '상담신청', to: 'consultation' },
-]
+export const PAGES = {
+  회사소개: { title: '회사소개', path: 'introduction' },
+  봉안당: { title: '봉안당', path: 'charnel_house' },
+  수목장: { title: '수목장', path: 'arboretum' },
+  봉안묘: { title: '봉안묘', path: 'mausoleum' },
+  상담신청: { title: '상담신청', path: 'consulting' },
+}
 
 const Header = () => {
   return (
     <header>
-      <h1>날들</h1>
+      <Link className={cx('logo')} to='/'>
+        <h1>날들</h1>
+      </Link>
       <nav>
         <ul>
-          {MENUS.map(({ title, to }) => (
+          {Object.values(PAGES).map(({ title, path }) => (
             <li key={title}>
-              <NavLink className={(isActive) => cx('nav', { isActive })} to={`/${to}`}>
-                {/* <NavLink className={styles.nav} to={`/${to}`}> */}
+              <NavLink className={(isActive) => cx('nav', { isActive })} to={`${path}`}>
                 {title}
               </NavLink>
             </li>
