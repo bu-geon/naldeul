ed0e4c056df4187845d3d7ecfd0451bd3c51bc6f
bugeon <bearbu@naver.com>
2022-10-22 15:33:24 +0900

    feat: add Consulting <button> in Footer

--- src/pages/_shared/Footer.tsx
+++ src/pages/_shared/Footer.tsx
@@ -1,9 +1,15 @@
+import classNames from 'classnames/bind'
+
+import styles from './footer.module.scss'
+
+const cx = classNames.bind(styles)
+
 const Footer = () => {
   return (
     <footer>
-      <button type='button'>상담 신청</button>
       <input type='text' placeholder='이름' />
       <input type='text' placeholder='010-6428-1270' />
+      <button type='button'>상담 신청</button>
     </footer>
   )
 }
